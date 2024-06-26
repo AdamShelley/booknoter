d6522bfdb1c78a980d811496ae82173b9c22d670
Adam Shelley <adam.shelley18@gmail.com>
2024-06-26 20:01:39 +0100

    Theming settings page

--- packages/renderer/src/pages/options.tsx
+++ packages/renderer/src/pages/options.tsx
@@ -1,7 +1,51 @@
-import React from "react";
-
 const OptionsPage = () => {
-  return <div>OptionsPage</div>;
+  return (
+    <div className="min-h-screen p-6 overflow-y-auto">
+      <h1 className="text-4xl font-bold text-center  mb-8">Options</h1>
+
+      <div className="rounded-lg p-6 overflow-y-auto">
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold text-slate-300 mb-2">Settings</h2>
+          <p className="text-gray-300">Coming soon...</p>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold text-slate-300 mb-2">Themes</h2>
+          <p className="text-gray-300">Coming soon...</p>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold text-slate-300 mb-2">Font</h2>
+          <p className="text-gray-300">Coming soon...</p>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold text-slate-300 mb-2">Font Size</h2>
+          <p className="text-gray-300">Coming soon...</p>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold text-slate-300 mb-2">Line Height</h2>
+          <p className="text-gray-300">Coming soon...</p>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold text-slate-300 mb-2">Margin</h2>
+          <p className="text-gray-300">Coming soon...</p>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold text-slate-300 mb-2">Padding</h2>
+          <p className="text-gray-300">Coming soon...</p>
+        </div>
+
+        <div>
+          <h2 className="text-2xl font-semibold text-slate-300 mb-2">Background Color</h2>
+          <p className="text-gray-300">Coming soon...</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OptionsPage;
